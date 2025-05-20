c7ecb913b177bd46bbd952c5abaca2c9733275c8
PinkyGames <147994391+Pinky-Core@users.noreply.github.com>
2025-05-20 14:44:37 -0300

    asd

--- Assets/Script/SceneChangeUI.cs
+++ Assets/Script/SceneChangeUI.cs
@@ -2,26 +2,33 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
+// Requiere que este GameObject tenga un componente Button
 [RequireComponent(typeof(Button))]
 public class SceneChangeUI : MonoBehaviour
 {
+    // Nombre de la escena a la que se va a cambiar al hacer clic
     [SerializeField] private string sceneToSwitchName;
 
+    // Referencia al componente Button
     private Button button;
-    
-    void Start ()
+
+    void Start()
     {
+        // Se obtiene el componente Button al iniciar
         button = GetComponent<Button>();
+
+        // Se le asigna el método LoadNext al evento de clic del botón
         button.onClick.AddListener(LoadNext);
     }
-    
-    void OnDestroy ()
+
+    void OnDestroy()
     {
+        // Se remueve el listener al destruir el objeto (buena práctica para evitar referencias colgantes)
         button.onClick.RemoveListener(LoadNext);
     }
-    
 
-    private void LoadNext ()
+    // Método que carga la nueva escena
+    private void LoadNext()
     {
         SceneManager.LoadScene(sceneToSwitchName);
     }
